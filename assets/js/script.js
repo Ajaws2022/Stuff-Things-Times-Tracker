a43e4e55f5ca704ae098e4c5b7995e83ec5ccc24
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-14 18:02:26 -0700

    added comments on how I want to structure the application

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// create a function that creates the js moment to display the current date and time
+
+// create a function that sets the text entry as an object in local storage when the save item is clicked
+
+// parse the object back to the list as a list item to make sure it stays on refresh
+
+// create an if statement that compares the current time to the list and changes the color based on past, present, or future
+
