c96561236f073f4b8655b392ad07c0c23b8a31e0
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-10-16 18:18:36 -0700

    added function to check time

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,38 @@
 // create an if statement that compares the current time to the list and changes the color based on past, present, or future
 
 //   var icon = $("#9am").button( "option", "icon" );
- function setTime(){
+ function setTime(Interval){
     var today = moment();
 
     $(".dateAndTime").text(today.format("MMM Do, YYYY  h:mm a"));
 }
 
 setInterval(setTime(), (1000 * 60) * 1);
+
+function checkTime(){
+    var currTime = moment().format('H')
+
+    var chronElements = $('.input')
+
+    for (var i = 0; i < chronElements.length; i++){
+
+        var elemID = chronElements[i].id;
+
+        var changeID = document.getElementById(chronElements[i].id)
+
+        $(chronElements[i].id).removeClass(".past .present .future");
+
+        if (elemID < currTime){
+            $(changeID).addClass('.past');
+        } else if (elemID > currTime){
+            $(changeID).addClass('.future');
+        } else {
+            $(changeID).addClass('.present');
+        }
+        
+
+        
+    }
+}
+
+checkTime();
